2cab475f44207c5df2dbee769a02d07376db01a7
赵鹏飞 <vim.zpf@gmail.com>
2024-07-28 18:14:13 +0800

    spring boot 环境搭建

---
+++ spring-boot-project/spring-boot-my/src/main/java/com/example/springbootmy/SpringBootMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyApplication.class, args);
+	}
+
+}
---
+++ spring-boot-project/spring-boot-my/src/test/java/com/example/springbootmy/SpringBootMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
